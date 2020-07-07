437dd107a77f998e2566faa169fb217bf97260cf
Emmanuel Egomson <femmor2u@gmail.com>
2020-07-07 11:04:41 -0500

    added the footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Switch, Route, Link } from "react-router-dom"
+import { Switch, Route } from "react-router-dom"
 
 // Components
 import Landing from "./components/Landing"
