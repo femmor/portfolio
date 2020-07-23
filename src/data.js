00b388e044f7da24e980d1aaf736112bb5df59c0
Emmanuel Egomson <femmor2u@gmail.com>
2020-07-22 20:50:16 -0500

    added another portfolio item

--- src/data.js
+++ src/data.js
@@ -6,6 +6,14 @@ export const portfolioData = [
         project_desc: "Github API job portal",
         image: "./images/gitjobs.jpg",
         tags: ["React", "NodeJS", "Github API", "React-Bootstrap", "Markdown", "Netlify", "Express"]
+    },
+    {
+        id: 4,
+        project_name: "Football Club Project",
+        project_link: "https://mancity-project.netlify.app",
+        project_desc: "Manchester city football club project",
+        image: "./images/mancity.jpg",
+        tags: ["React", "NodeJS", "Material-UI", "React Move", "Firebase", "Express", "Netlify"]
     }
     
 ]
